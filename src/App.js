ed18bb1939db67abcd63db102fd2e41ccf93fdb7
NaVnEeTsInG <93895351+NaVnEeTsInG@users.noreply.github.com>
2023-12-14 14:19:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>hfjfgkjgjgjkkjtuytu5657
-          hhhhhhhhhhhh aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-          jbjbv ak vjdbv Edit <code>src/App.js</code> and save to reload.
+        <p>
+        Edit <code>src/App.js</code> and save to reload.
         </p>
        
          
