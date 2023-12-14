731e6892cad9b875fe61aa0fb6143d318c5e6be1
NaVnEeTsInG <93895351+NaVnEeTsInG@users.noreply.github.com>
2023-12-14 14:14:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,25 +6,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>hfjfgkjgjgjkkjtuytu5657
           hhhhhhhhhhhh aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
           aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
           aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
           jbjbv ak vjdbv Edit <code>src/App.js</code> and save to reload.
         </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          */}
+       
          
         <Cmp1></Cmp1> 
-        
+
           Learn React
-        </a>
+        
       </header>
     </div>
   );
